2d4bc5a2f25cdf7843bd07c0c27c2185c246087a
Qadir Husainee <abdul.husain@nitorinfotech.com>
2018-08-11 09:07:12 +0530

    Initial commit

---
+++ src/js/Actions/userActions.js
@@ -0,0 +1,31 @@
+/*export function fetchUser() {
+  return {
+    type: "FETCH_USER"
+          }
+        }
+
+export function setUserName(user_details) {
+   console.log('console user actions',user_details)
+  return {
+   type: 'SET_USER_NAME',
+    payload: user_details,
+  }
+}
+
+export function setUserPwd(pwd) {
+  return {
+    type: 'SET_USER_PWD',
+    payload: pwd,
+  }
+}
+export function setUserdetails(name,pwd,email) {
+  return {
+    type: 'SET_USER_DETAILS',
+    payload: {
+    name,
+    pwd,
+    email
+  }
+  }
+}
+*/
